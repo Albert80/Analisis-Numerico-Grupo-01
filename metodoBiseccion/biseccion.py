e1e2d54b0b29db8de1bb4be25032c756bee08b73
Albert <cesaratj27@gmail.com>
2014-02-10 08:08:20 -0600

    bisección en línea

---
+++ metodoBiseccion/biseccion.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''	UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO
+	FACULTAD DE INGENIERÍA
+	Análisis Numérico
+	Semestre: 2014-2
+	Profesor: Ingeniero Alejandro Figueroa 
+	Grupo: 01
+	Método Bisección
+	Viernes 7 de Febrero 2014
+	Created by: César Alberto Trejo Juárez'''
+
