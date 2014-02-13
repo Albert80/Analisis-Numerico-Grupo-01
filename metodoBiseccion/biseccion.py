f72c00c0701ee755cb395179cbbfc2066d535a0e
Albert <cesaratj27@gmail.com>
2014-02-12 21:43:49 -0600

    bisección en línea

--- metodoBiseccion/biseccion.py
+++ metodoBiseccion/biseccion.py
@@ -11,3 +11,23 @@
 	Viernes 7 de Febrero 2014
 	Created by: César Alberto Trejo Juárez'''
 
+import math
+
+def ecuation(x):
+	pow(x, 3.5) - 80
+
+def biseccion(tol):
+	a = 3
+	b = 4
+	c = 0
+	while(math.fabs(ecuation(a-b)) >= tol and ecuation(c) != 0):
+		c = (a + b) / 2
+		print 'Limite a: ' + str(a) + '	Límite b: ' + str(b) + 'valor medio: ' + str(c) + ''
+		if ecuation(a) * ecuation(c) < 0:
+			b = c
+
+		else:
+			a = c
+
+	print 'Raíz encontrada: ' + str(c)
+
