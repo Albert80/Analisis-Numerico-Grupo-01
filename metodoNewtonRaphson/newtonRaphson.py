d32859920fd9a29af8c172756b68d19a5ac1b890
Albert <cesaratj27@gmail.com>
2014-02-13 23:06:20 -0600

    tarea 5.10 online

--- metodoNewtonRaphson/newtonRaphson.py
+++ metodoNewtonRaphson/newtonRaphson.py
@@ -14,29 +14,33 @@
 import math
 
 def ecuation(x):
-	return (math.sin(x) - pow(x, 2))
+	return (pow(x, 4) - 8 * pow(x, 3) - 35 * pow(x, 2) + 450 * x - 1001)
 
 def derivateEcuation(x):
-	return (math.cos(x) - 2 * x)
+	return (4 * pow(x,3) - 24 * pow(x,2) - 70 * x + 450 )
 
 def newtonRaphson(tol):
-	x_0 = 0.5
+	x_0 = 3
 	x_n_1 = 0
 	tolerancia = tol
 	x_n = x_0
 	it = 1
 	root = 0
-	while (math.fabs(x_n_1 - x_n) < tolerancia) or (ecuation(x_n_1) == 0):
+	newValue = 0
+	beforeValue = 0
+	errorAbs = 0
+	erorrRel = 0
+	while (math.fabs(x_n_1 - x_n) < tolerancia) or (ecuation(x_n_1) != 0):
 		x_n_1 = x_n - (ecuation(x_n) / derivateEcuation(x_n))
 		print 'Iteración: ' + str(it) + '	valor inicial: ' + str(x_n) + '	f(x): ' + str(ecuation(x_n)) + "	f'(x): " + str(derivateEcuation(x_n)) + '	posible raíz: ' + str(x_n_1)
-		x_n = x_n_1
-		it += 1
+		x_n = math.fabs(x_n_1)
 		if it > 2:
-			if x_n_1 != root:
-				root = x_n_1
-			else:
+			print 'Error absoluto: ' + str() + '	Error relativo: ' + str()
+			if x_n_1 == root:
 				print 'Raíz encontrada:	' + str(root)
 				break
+			root = math.fabs(x_n_1)		
+		it += 1
 
 
 # Examples
